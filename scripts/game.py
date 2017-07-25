cd0286860b186b5c1bbfd369e36da8edfbdfcc53
creeperdeking <alexis.gros99@gmail.com>
2017-07-25 22:32:46 +0200

    oups

--- scripts/game.py
+++ scripts/game.py
@@ -255,7 +255,7 @@ class Game:
 	def ridePath(self, element):
 		isOpen = False
 		genericElement = element.split('_')[0]
-		pdb.set_trace()
+		
 		# This is the stack of the tiles that have to be dealed with
 		currentTileStack = [ [self.currentTile, self.currentTile.elements[element]] ]
 		#This is the old tiles already done:
